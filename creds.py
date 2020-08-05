e289384e30967b2818809a93ad77ac831bb5542d
Suyash Jain <53927129+Suyashjain05@users.noreply.github.com>
2020-08-05 19:26:31 +0530

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,7 @@
+class my ():
+    BOT_TOKEN ="1260760035:AAGPRoRk2YBdM-on_-wiw0AamqWhY1srvys"
+    API_ID = 996009
+    API_HASH = "1bda4304eebe484ea9197653a2663c98" 
+    MID = "JziCwu70940269596268"
+    KEY = "3TF&j67uHPEEuK@f"
+    LINK = "https://securegw.paytm.in/link/create"
